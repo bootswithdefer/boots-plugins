47d50e92293afcf6a3dd575ecd3d557a005491c1
bootswithdefer <jdefer@gmail.com>
2010-12-08 11:18:36 -0700

    add debugging, hmod version check

--- VersionCheck/VersionCheck.java
+++ VersionCheck/VersionCheck.java
@@ -8,6 +8,7 @@ import java.net.URLConnection;
 public class VersionCheck
 {
 	static final Logger log	= Logger.getLogger("Minecraft");
+	private boolean debug = false;
 
 	VersionCheck(String name, int version)
 	{
@@ -16,6 +17,7 @@ public class VersionCheck
 		PropertiesFile properties = new PropertiesFile("server.properties");
 		try {
 			check = properties.getBoolean("boots-version-check", true);
+			debug = properties.getBoolean("boots-debug", false);
 		} catch (Exception ex) {
 			log.log(Level.SEVERE, "Exception while reading from server.properties", ex);
 		}
@@ -40,16 +42,35 @@ public class VersionCheck
 		public void run()
 		{
 			try {
+				Thread.sleep(5000);
 				URL url = new URL("http://www.bootswithdefer.com/minecraft/version.php?plugin=" + name + "&version=" + version + "&hmod=" + etc.getInstance().getVersion());
 				URLConnection urlc = url.openConnection();
 				
 				BufferedReader in = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-				String line = in.readLine();
+				String verline = in.readLine();
+				String hModline = in.readLine();
 				in.close();
 
-				int ver = Integer.parseInt(line);
-				if (ver > version)
-					log.info(name + " v" + ver + " available (you have v" + version + ")");
+				int serverVersion = 0;
+				int hModVersion = 0;
+				try {
+					serverVersion = Integer.parseInt(verline);
+					hModVersion = Integer.parseInt(hModline);
+				} catch (NumberFormatException ex) {
+				}
+
+				if (serverVersion > version)
+					log.info(name + " new version v" + serverVersion + " available (you have v" + version + ")");
+				if (debug)
+				{
+					if (serverVersion < version)
+						log.info(name + " v" + serverVersion + " is latest reported, you have newer v" + version);
+					else
+						log.info(name + " matches latest version (v" + serverVersion + ")");
+				}
+				
+				if (version == serverVersion && etc.getInstance().getVersion() < hModVersion)
+					log.log(Level.SEVERE, name + " v" + version + " is not supported on your version of hMod (" + etc.getInstance().getVersion() + "), upgrade to at least hMod " + hModVersion);
 			} catch (Exception ex) {
 				log.log(Level.SEVERE, "VersionCheck exception", ex);
 			}
